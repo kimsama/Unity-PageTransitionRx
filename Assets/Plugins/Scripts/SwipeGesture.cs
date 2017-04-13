190eca5553caaf8a27b3d8e5f5b5d2fcba86a185
kimsama <hwpeterkim@gmail.com>
2017-04-13 19:22:54 +0900

    added DOTween and Scripts/SwipeGesture.cs and Scripts/TurnPage.cs

---
+++ Assets/Plugins/Scripts/SwipeGesture.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class SwipeGesture : MonoBehaviour
+{
+    public float TotalSeconds = 1.0f;
+    public float ThresholdSenconds = 1.0f;
+    public float ThresholdDistance = 100.0f;
+
+    private Subject<Unit> onSwipeLeft = new Subject<Unit>();
+    public IObservable<Unit> OnSwipeLeft
+    {
+        get { return onSwipeLeft; }
+    }
+
+    private Subject<Unit> onSwipeRight = new Subject<Unit>();
+    public IObservable<Unit> OnSwipeRight
+    {
+        get { return onSwipeRight; }
+    }
+
+    private Subject<Unit> onSwipeDown = new Subject<Unit>();
+    public IObservable<Unit> OnSwipeDown
+    {
+        get { return onSwipeDown; }
+    }
+
+    private Subject<Unit> onSwipeUp = new Subject<Unit>();
+    public IObservable<Unit> OnSwipeUp
+    {
+        get { return onSwipeUp; }
+    }
+
+    private Vector2 beginPosition;
+    private DateTime beginTime;
+
+    void OnEnable() 
+    { 
+        var  eventTrigger = this.gameObject.AddComponent < ObservableEventTrigger > (); 
+
+        eventTrigger 
+            . OnBeginDragAsObservable () 
+            . TakeUntilDisable ( this ) 
+            . Where ( eventData => eventData.pointerDrag.gameObject == this.gameObject ) 
+            . Select ( eventData => eventData . position ) 
+            . Subscribe ( position => 
+            { 
+                this . beginPosition =  position ; 
+                this . beginTime  =  DateTime.Now ; 
+            }); 
+
+        var  onEndDragObservable  =  eventTrigger 
+            . OnEndDragAsObservable () 
+            . TakeUntilDisable ( this ) 
+            . Where ( eventData => (DateTime.Now - this.beginTime).TotalSeconds < this.ThresholdSenconds ) 
+            . Select ( eventData  => eventData.position ) 
+            . Share (); 
+
+        // left 
+        onEndDragObservable 
+            . Where ( position  =>  beginPosition.x  >  position.x ) 
+            . Where ( position  =>  Mathf . Abs ( beginPosition . x  -  position.x ) >= this.ThresholdDistance ) 
+            . Subscribe ( _  =>  onSwipeLeft . OnNext ( Unit.Default )) ; 
+
+        // right 
+        onEndDragObservable 
+            . Where ( position => position.x  >  beginPosition . x ) 
+            . Where ( position => Mathf . Abs ( position . x  -  beginPosition.x ) >= this.ThresholdDistance ) 
+            . Subscribe ( _  => onSwipeRight . OnNext ( Unit.Default )); 
+
+        // down 
+        onEndDragObservable 
+            . Where ( position => beginPosition.y > position.y ) 
+            . Where ( position => Mathf.Abs (beginPosition.y  -  position.y ) >= this.ThresholdDistance ) 
+            . Subscribe ( _ => onSwipeDown.OnNext (Unit.Default)); 
+
+        // up 
+        onEndDragObservable 
+            . Where ( position => position.y > beginPosition.y ) 
+            . Where ( position => Mathf . Abs ( position.y - beginPosition.y ) >=  this.ThresholdDistance ) 
+            . Subscribe ( _ => onSwipeUp.OnNext (Unit.Default)); 
+
+    }
+}
---
+++ Assets/Plugins/Scripts/TurnPage.cs
@@ -0,0 +1,57 @@
+﻿using DG.Tweening;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SwipeGesture), typeof(HorizontalLayoutGroup))]
+public class TurnPage : MonoBehaviour
+{
+    public float PageWidth = 1080;
+
+    private RectTransform rectTransform;
+    private SwipeGesture swipeGesture;
+    private Tween moveAnimation;
+    private int pageCount;
+    private int currentPage = 1;
+
+    void Awake()
+    {
+        DOTween.Init();
+        DOTween.defaultAutoPlay = AutoPlay.None;  // Tween 생성시 자동 실행시키지 
+    }
+
+    void OnEnable() 
+    { 
+        this.rectTransform  =  this.GetComponent<RectTransform>(); 
+        this.swipeGesture  =  this.GetComponent<SwipeGesture>(); 
+        this.pageCount  =  this.transform.childCount ; 
+
+        // next 
+        this . swipeGesture 
+            . OnSwipeLeft 
+            . Where ( _  =>  currentPage  <  pageCount )  // 최대 페이지 이전 인 경우에만 진행 
+            . Where ( _  =>  this.moveAnimation  ==  null  ||  ! this.moveAnimation.IsPlaying ())  // 애니메이션 실행 중이 아니 
+            . Subscribe ( _  => 
+            { 
+                this.currentPage ++; 
+                this.moveAnimation  =  this.rectTransform 
+                    .DOAnchorPosX ( rectTransform.anchoredPosition.x  -  this.PageWidth,  1.0f ) 
+                    .SetEase ( Ease.OutBounce ) 
+                    .Play (); 
+            }); 
+
+        // back 
+        this . swipeGesture 
+            . OnSwipeRight 
+            . Where ( _  =>  currentPage  >  1 )  // 1 페이지 이상인 경우에만 돌아갈 
+            . Where ( _  =>  this.moveAnimation == null || !this.moveAnimation.IsPlaying())  // 애니메이션 실행 중이 없다 
+            . Subscribe ( _  => 
+            { 
+                this.currentPage--; 
+                this.moveAnimation = this.rectTransform 
+                    .DOAnchorPosX (rectTransform.anchoredPosition.x  +  this.PageWidth, 1.0f) 
+                    .SetEase (Ease.OutBounce) 
+                    .Play(); 
+            } ); 
+    }
+}
